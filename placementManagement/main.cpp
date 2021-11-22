601e889a3d3ae46d0c87eb0b40187ae04b1279a7
IshuGupta02 <ishu_g@cs.iitr.ac.in>
2021-11-22 15:52:13 +0530

    Added main.cpp

---
+++ placementManagement/main.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#include<string>
+#include "placementManagement/User.h"
+#include "placementManagement/College/college.h"
+#include "placementManagement/company/company.h"
+#include "placementManagement/interview/interview.h"
+
+using namespace std;
+
+int main() {
+	return 0;
+}
